c557f537f3b817e1a1260bc3ffe54099f5ca66fe
EastwindRT <renjithrt@hotmail.com>
2024-08-23 01:38:49 +0000

    Updated configuration for Cloudflare Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,31 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    postcss: './postcss.config.js',
+  },
   server: {
     host: '0.0.0.0',
-  }
+    hmr: {
+      clientPort: 443,
+    },
+  },
+  esbuild: {
+    loader: 'jsx',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
